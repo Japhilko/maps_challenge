d571d0f49c87436c206d4136fb2d271cf937b84a
Japhilko <JanPhilipp.kolb@gmail.com>
2020-11-18 20:49:41 +0100

    landuse

--- rcode/maps_challenge_day18_landuse.R
+++ rcode/maps_challenge_day18_landuse.R
@@ -6,3 +6,38 @@ data(World)
 tm_shape(land,  relative=FALSE) +
   tm_raster("cover_cls", title="Global Land Cover")
 
+
+library(osmplotr)
+library(tmaptools)
+library(tmap)
+
+gc <- geocode_OSM("Berlin")
+bbox <- get_bbox (gc$bbox)
+dat_osm <- extract_osm_objects (key="landuse", bbox = bbox)
+
+# save(dat_osm,file=paste0("C:/Arbeit/geoscience/data/","dat_osm_berlin_landuse.Rdata"))
+
+qtm(dat_osm,"landuse")
+
+#############
+
+gc <- geocode_OSM("Mannheim")
+bbox <- get_bbox (gc$bbox)
+dat_osm <- extract_osm_objects (key="landuse", bbox = bbox)
+
+save(dat_osm,file=paste0("C:/Arbeit/geoscience/data/dat_osm_mannheim_landuse.Rdata"))
+
+qtm(dat_osm,"landuse")
+
+
+
+dat_osm2 <- dat_osm[dat_osm$landuse%in%names(sort(table(dat_osm$landuse),decreasing = T))[1:20],]
+
+?tmap_options
+
+tmap_mode("view")
+
+tm_shape(dat_osm2) + tm_polygons("landuse") 
+
+
+#30daymapcallenge day 18 landuse - a map of #Mannheim in Germany. I used the #rstats packages osmplotr and tmap. The data is from #Openstreetmap. My code is at https://github.com/Japhilko/maps_challenge #rspatial #r   
