d0831518ec453fa0c32c89792291a7f6e3f73257
Japhilko <JanPhilipp.kolb@gmail.com>
2020-11-23 13:08:06 +0100

    day 22 movement

--- rcode/maps_challenge_day21_water.R
+++ rcode/maps_challenge_day21_water.R
@@ -2,3 +2,36 @@
 # Day 21 - map with water
 # Jan-Philipp Kolb
 # Wed Oct 28 16:32:31 2020
+
+
+library(osmplotr)
+
+gc <- geocode_OSM("Saarland")
+bbox <- get_bbox (gc$bbox)
+dat_osm <- extract_osm_objects (key="natural",value="water", bbox = bbox)
+
+save(dat_osm,file=paste0("C:/Arbeit/geoscience/data/","dat_osm_saarland_water.Rdata"))
+
+
+gc <- geocode_OSM("Deutschland")
+bbox <- get_bbox (gc$bbox)
+dat_osm <- extract_osm_objects (key="natural",value="water", bbox = bbox)
+
+save(dat_osm,file=paste0("C:/Arbeit/geoscience/data/","dat_osm_saarland_water.Rdata"))
+
+png("maps/maps_challenge_day21.png")
+qtm(dat_osm,fill = "blue",borders = "blue")
+dev.off()
+
+
+
+#30daymapchallenge day 21 - water. #Openstreetmap search with #rstats package osmplotr with key=natural and value=water for Saarland. #rspatial
+
+
+# Links -------------------------------------------------------------------
+
+
+# https://wiki.openstreetmap.org/wiki/Map_Features#Water_related
+
+
+# https://www.r-spatial.org/r/2018/10/25/ggplot2-sf.html
---
+++ rcode/maps_challenge_day22_movement.R
@@ -0,0 +1,56 @@
+# 30 Day map challenge - Day 22 - movement
+# Jan-Philipp Kolb
+# 22.11.2020
+
+
+library(osmplotr)
+
+gc <- geocode_OSM("Barcelona")
+bbox <- get_bbox (gc$bbox)
+dat_osm <- extract_osm_objects (key="route",value="subway", bbox = bbox)
+
+
+qtm(dat_osm,borders="ref")
+
+
+png("maps/maps_challenge_day22.png")
+ggplot() + geom_sf(data = dat_osm,aes(col=ref))
+dev.off()
+
+
+#30daymapchallenge day 22: movement. For today I have the Metro lines of Barcelona. I used the #rstats packages #ggplot2 and osmplotr. #rspatial
+# https://bookdown.org/yihui/rmarkdown-cookbook/animation.html
+
+
+
+# Further attempts --------------------------------------------------------
+
+
+library(move)
+library(hflights)
+
+library(gganimate)
+
+# install.packages("hflights")
+# install.packages("gganimate")
+
+
+# https://github.com/anitagraser/movingpandas
+
+head(hflights)
+
+
+
+# Links -------------------------------------------------------------------
+
+# https://wiki.openstreetmap.org/wiki/Map_Features#Roads
+
+# https://ggplot2.tidyverse.org/reference/ggsf.html
+
+# https://bookdown.org/yihui/rmarkdown-cookbook/animation.html
+
+# https://anitagraser.com/movement-data-in-gis/
+
+# https://www.esri.com/en-us/arcgis/products/data/data-portfolio/business-behavioral
+
+# https://cran.r-project.org/web/packages/fishmove/fishmove.pdf
