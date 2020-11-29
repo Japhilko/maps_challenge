de83ca0eff855d455bd363091747943f8cd961dd
Japhilko <JanPhilipp.kolb@gmail.com>
2020-11-29 18:10:13 +0100

    globe

---
+++ rcode/maps_challenge_day28_non_geographical_maps.R
@@ -0,0 +1,33 @@
+# Non geographical maps
+# Jan-Philipp Kolb
+# Sun Nov 29 07:49:11 2020
+
+# code from https://cran.r-project.org/web/packages/ggfortify/vignettes/plot_pca.html
+
+# pca
+
+library(ggfortify)
+df <- iris[1:4]
+pca_res <- prcomp(df, scale. = TRUE)
+
+autoplot(pca_res, data = iris, colour = 'Species')
+
+
+
+png("maps/maps_challenge_day28_ngeomap.png")
+autoplot(pca_res, data = iris, colour = 'Species',
+         loadings = TRUE, loadings.colour = 'blue',
+         loadings.label = TRUE, loadings.label.size = 3)
+dev.off()
+
+
+#30daymapchallenge day 28 a non geographical map. Here is the result of a principal component analysis on the iris dataset. I used the 
+#rstats package ggfortify for this. Thanks to Masaaki Horikoshi, @TerryTangYuan, Austin Dickey and many more.
+
+
+
+
+# https://towardsdatascience.com/how-to-build-a-non-geographical-map-1-8d3373e83d6c
+
+# https://stackoverflow.com/questions/47788171/display-non-geographical-maps-using-r-leaflet-shiny
+
---
+++ rcode/maps_challenge_day29_globe.R
@@ -0,0 +1,16 @@
+#30daymapchallenge
+# Jan-Philipp Kolb
+# 
+
+# install.packages("globe")
+
+library(globe)
+
+globeearth()
+
+png("maps/maps_challenge_day29_globe.png")
+    globeearth(eye=place("everest"),col="royalblue")
+dev.off()
+
+
+#30daymapchallenge day 29. topic today is the globe. I used the #rstats package globe. Thanks to Adrian Baddeley, Thomas Lawrence and @egerubak #rspatial 
